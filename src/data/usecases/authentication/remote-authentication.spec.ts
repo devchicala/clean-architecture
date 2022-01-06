21ef4aeaa93e66966ca62ba2b81550a77b02c2db
devchicala <delciofrancisco1@gmail.com>
2022-01-06 22:57:34 +0000

    feat: initialization first test

--- src/data/usecases/authentication/remote-authentication.spec.ts
+++ src/data/usecases/authentication/remote-authentication.spec.ts
@@ -1,5 +1,25 @@
+interface HttpPostClient{
+  post: (url: string) => Promise<void>
+}
+
+class RemoteAuthentication {
+  constructor (private readonly url: string,
+    private readonly httpClient: HttpPostClient) {}
+
+  async auth (): Promise<void> {
+    return await Promise.resolve()
+  }
+}
+
 describe('RemoteAuthentication', () => {
-  test('should ', () => {
-    
+  test('Should call HttpClient with correct URL', async () => {
+    class HttpClientSpy implements HttpPostClient {
+
+    }
+    const url = 'any_url'
+    const httpClient = HttpClient()
+    const sut = new RemoteAuthentication(url)
+    await sut.auth()
+    expect(httpClient.url).toBe(url)
   })
 })
